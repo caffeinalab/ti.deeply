54c8fe4b3e83ad4ac19d902c7c423a8b689de0cd
Andrea Jonus <j.jonus.j@gmail.com>
2018-08-02 14:56:51 +0200

    Added handler activity, theme, and manifest skeleton.

---
+++ android/src/ti/deeply/DeepLinkHandlerActivity.java
@@ -0,0 +1,60 @@
+package ti.deeply;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.appcelerator.titanium.TiApplication;
+
+import java.util.HashMap;
+
+import org.appcelerator.kroll.KrollRuntime;
+
+public class DeepLinkHandlerActivity extends Activity {
+
+	private static String LCAT = "ti.deeply.DeepLinkHandlerActivity";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		try {
+			Log.d(LCAT, "started");
+			super.onCreate(savedInstanceState);
+			finish();
+
+			TiDeeplyModule module = TiDeeplyModule.getModule();
+			Context context = getApplicationContext();
+			Intent intent = getIntent();
+			String data = intent.getStringExtra(TiDeeplyModule.INTENT_DATA);
+			String action = intent.getStringExtra(TiDeeplyModule.INTENT_ACTION);
+			Bundle extras = intent.getBundleExtra(TiDeeplyModule.INTENT_EXTRAS);
+
+			Intent launcherIntent = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName());
+			launcherIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+			launcherIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+			if (module.hasMessageCallback() && KrollRuntime.getInstance().getRuntimeState() != KrollRuntime.State.DISPOSED) {
+				module.sendMessage(data, action, extras, true);
+			} else {
+				launcherIntent.putExtra(TiDeeplyModule.INTENT_DATA, data);
+				launcherIntent.putExtra(TiDeeplyModule.INTENT_ACTION, action);
+				launcherIntent.putExtra(TiDeeplyModule.INTENT_EXTRAS, extras);
+			}
+
+			startActivity(launcherIntent);
+
+		} catch (Exception e) {
+			// noop
+		} finally {
+			finish();
+		}
+	}
+
+	@Override protected void onResume() {
+		Log.d(LCAT, "resumed");
+		super.onResume();
+	}
+
+}
--- android/src/ti/deeply/TiDeeplyModule.java
+++ android/src/ti/deeply/TiDeeplyModule.java
@@ -8,57 +8,100 @@
  */
 package ti.deeply;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.appcelerator.kroll.KrollFunction;
 import org.appcelerator.kroll.KrollModule;
 import org.appcelerator.kroll.annotations.Kroll;
 
 import org.appcelerator.titanium.TiApplication;
+
+import android.os.Bundle;
+
 import org.appcelerator.kroll.common.Log;
 import org.appcelerator.kroll.common.TiConfig;
 
 
-@Kroll.module(name="TiDeeply", id="ti.deeply")
+@Kroll.module(name="TiDeeply", id="ti.deeply", propertyAccessors = {
+	"callback"
+})
 public class TiDeeplyModule extends KrollModule
 {
 
 	// Standard Debugging variables
-	private static final String LCAT = "TiDeeplyModule";
+	private static final String LCAT = "ti.deeply.TiDeeplyModule";
 	private static final boolean DBG = TiConfig.LOGD;
+
+	public static final String INTENT_DATA = "tideeply.data";
+	public static final String INTENT_ACTION = "tideeply.action";
+	public static final String INTENT_EXTRAS = "tideeply.extras";
+
+	private static TiDeeplyModule module = null;
+
+	private KrollFunction deepLinkCallback = null;
 
 	// You can define constants with @Kroll.constant, for example:
 	// @Kroll.constant public static final String EXTERNAL_NAME = value;
 
-	public TiDeeplyModule()
-	{
+	public TiDeeplyModule() {
 		super();
+		module = this;
 	}
 
-	@Kroll.onAppCreate
-	public static void onAppCreate(TiApplication app)
-	{
-		Log.d(LCAT, "inside onAppCreate");
-		// put module init code that needs to run when the application is created
+	public static TiDeeplyModule getModule() {
+		if (module != null) return module;
+		else return new TiDeeplyModule();
 	}
 
-	// Methods
+	public void parseBootIntent() {
+		try {
+			Intent intent = TiApplication.getAppRootOrCurrentActivity().getIntent();
+			String data = intent.getData();
+			String action = intent.getAction();
+			Bundle extras = intent.getExtras();
+
+			if (data != null) {
+				sendDeepLink(data, action, extras, true);
+				intent.removeExtra(INTENT_DATA);
+				intent.removeExtra(INTENT_ACTION);
+				intent.removeExtra(INTENT_EXTRAS);
+			} else {
+				Log.d(LCAT, "Empty data in Intent");
+			}
+		} catch (Exception ex) {
+			Log.e(LCAT, "parseBootIntent" + ex);
+		}
+	}
+
+	public void sendDeepLink(String data, String action, Bundle extras, Boolean inBackground) {
+		if (deepLinkCallback == null) {
+			Log.e(LCAT, "sendMessage invoked but no deepLinkCallback defined");
+			return;
+		}
+
+		HashMap<String, Object> e = new HashMap<String, Object>();
+		e.put("data", data); // to parse on reverse on JS side
+		e.put("action", action);
+		e.put("extras", convertBundleToMap(extras));
+		e.put("inBackground", inBackground);
+
+		deepLinkCallback.callAsync(getKrollObject(), e);
+	}
+
+	private static Map<String, Object> convertBundleToMap(Bundle bundle) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		Iterator<String> keys = bundle.keySet().iterator();
+		while (keys.hasNext()) {
+			String key = keys.next();
+			map.put(key, bundle.get(key));
+		}
+		return map;
+	}
+
 	@Kroll.method
-	public String example()
-	{
-		Log.d(LCAT, "example called");
-		return "hello world";
-	}
-
-	// Properties
-	@Kroll.getProperty
-	public String getExampleProp()
-	{
-		Log.d(LCAT, "get example property");
-		return "hello world";
-	}
-
-
-	@Kroll.setProperty
-	public void setExampleProp(String value) {
-		Log.d(LCAT, "set example property: " + value);
+	public void setDeepLinkCallback(KrollFunction callback) {
+		deepLinkCallback = callback;
 	}
 
 }
